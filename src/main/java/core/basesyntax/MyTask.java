0058a6e305cf17834c7d702aea7cc5f455981e35
vitalii_yelinskyi <yelinskyizp@gmail.com>
2021-11-01 21:53:19 +0200

    frok join pool - threads

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,10 +1,13 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
-    private int startPoint;
-    private int finishPoint;
+    private static final int MAX_DISTANCE_FOR_ONE_THREAD = 10;
+    private final int startPoint;
+    private final int finishPoint;
 
     public MyTask(int startPoint, int finishPoint) {
         this.startPoint = startPoint;
@@ -13,7 +16,31 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        long sum = 0;
+        if (finishPoint - startPoint > MAX_DISTANCE_FOR_ONE_THREAD) {
+            List<RecursiveTask<Long>> subTasks = new ArrayList<>(createSubTasks());
+            for (RecursiveTask<Long> subTask : subTasks) {
+                subTask.fork();
+            }
+            for (RecursiveTask<Long> subTask : subTasks) {
+                sum += subTask.join();
+            }
+            return sum;
+        } else {
+            for (int i = startPoint; i < finishPoint; i++) {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+
+    private List<RecursiveTask<Long>> createSubTasks() {
+        List<RecursiveTask<Long>> subTask = new ArrayList<>();
+        int finishForFirstThreadStartForSecond = startPoint + MAX_DISTANCE_FOR_ONE_THREAD;
+        RecursiveTask<Long> first = new MyTask(startPoint, finishForFirstThreadStartForSecond);
+        RecursiveTask<Long> second = new MyTask(finishForFirstThreadStartForSecond, finishPoint);
+        subTask.add(first);
+        subTask.add(second);
+        return subTask;
     }
 }
