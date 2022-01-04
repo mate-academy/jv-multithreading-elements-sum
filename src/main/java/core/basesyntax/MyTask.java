da3da0e1fe054d483dbff1d1c35e1e73ee5b4438
Andriy-dz <andriydzdz@gmail.com>
2022-01-04 12:49:11 +0200

    implemented MyTask

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
@@ -13,7 +15,32 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        if (finishPoint - startPoint > 10) {
+            List<RecursiveTask<Long>> recursiveTasks = new ArrayList<>(createSubTask());
+            for (RecursiveTask<Long> recursiveTask : recursiveTasks) {
+                recursiveTask.fork();
+            }
+            Long res = 0L;
+            for (RecursiveTask<Long> recursiveTask : recursiveTasks) {
+                res += recursiveTask.join();
+            }
+            return res;
+        } else {
+            Long res = 0L;
+            for (int i = startPoint; i < finishPoint; i++) {
+                res += i;
+            }
+            return res;
+        }
+    }
+
+    private List<RecursiveTask<Long>> createSubTask() {
+        List<RecursiveTask<Long>> recursiveActions = new ArrayList<>();
+        int difference = finishPoint - startPoint;
+        RecursiveTask<Long> first = new MyTask(startPoint, finishPoint - difference / 2);
+        RecursiveTask<Long> second = new MyTask(finishPoint - difference / 2, finishPoint);
+        recursiveActions.add(first);
+        recursiveActions.add(second);
+        return recursiveActions;
     }
 }
