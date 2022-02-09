09864679be20bdaddc26746776dda5bc0774640a
Dany Nesterov <nesterov1200997@gmail.com>
2022-02-09 20:50:39 +0200

    implemented task

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
@@ -12,8 +14,49 @@ public class MyTask extends RecursiveTask<Long> {
     }
 
     @Override
-    protected Long compute() {
-        // write your code here
-        return null;
+    public Long compute() {
+        long result = 0;
+        if (finishPoint > 0 && startPoint > 0 && (finishPoint - startPoint) > 10) {
+            List<RecursiveTask<Long>> recursiveTasks = new ArrayList<>(getRecursiveTasks());
+            for (RecursiveTask<Long> recursiveTask : recursiveTasks) {
+                recursiveTask.fork();
+            }
+        }
+        if (finishPoint < 0 && startPoint < 0 && (finishPoint - startPoint) > 10) {
+            List<RecursiveTask<Long>> recursiveTasks =
+                    new ArrayList<>(getRecursiveTasksNegativeNumbers());
+            for (RecursiveTask<Long> recursiveTask : recursiveTasks) {
+                recursiveTask.fork();
+            }
+        }
+        if (finishPoint > 0 && startPoint < 0 && (finishPoint + startPoint) > 10) {
+            List<RecursiveTask<Long>> recursiveTasks = new ArrayList<>(getRecursiveTasks());
+            for (RecursiveTask<Long> recursiveTask : recursiveTasks) {
+                recursiveTask.fork();
+            }
+        } else {
+            for (int i = startPoint; i < finishPoint; i++) {
+                result += i;
+            }
+        }
+        return result;
+    }
+
+    private List<RecursiveTask<Long>> getRecursiveTasksNegativeNumbers() {
+        List<RecursiveTask<Long>> recursiveTasks1 = new ArrayList<>();
+        RecursiveTask<Long> first = new MyTask(startPoint + 10, finishPoint + 10);
+        RecursiveTask<Long> second = new MyTask(startPoint + 10, finishPoint + 10);
+        recursiveTasks1.add(first);
+        recursiveTasks1.add(second);
+        return recursiveTasks1;
+    }
+
+    private List<RecursiveTask<Long>> getRecursiveTasks() {
+        List<RecursiveTask<Long>> recursiveTasks2 = new ArrayList<>();
+        RecursiveTask<Long> first = new MyTask(startPoint + 10, finishPoint - 10);
+        RecursiveTask<Long> second = new MyTask(startPoint + 10, finishPoint - 10);
+        recursiveTasks2.add(first);
+        recursiveTasks2.add(second);
+        return recursiveTasks2;
     }
 }
