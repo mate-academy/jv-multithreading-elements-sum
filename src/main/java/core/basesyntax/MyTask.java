0512fc2696c04cbbec2891ad38edff370d3d02d2
Dima Petrushenko <dmytro.petrushenko@gmail.com>
2022-03-14 16:28:19 +0200

    task done

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,5 +1,8 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
@@ -14,6 +17,36 @@ public class MyTask extends RecursiveTask<Long> {
     @Override
     protected Long compute() {
         // write your code here
-        return null;
+        if (finishPoint - startPoint > 10) {
+            List<RecursiveTask<Long>> subTasks = new ArrayList<>(createSubTasks());
+            for (RecursiveTask<Long> subTask : subTasks) {
+                subTask.fork();
+            }
+            long result = 0;
+            for (RecursiveTask<Long> subTask : subTasks) {
+                result += subTask.join();
+            }
+            return result;
+        }
+        return additionSubTask();
     }
+
+    private long additionSubTask() {
+        long result = 0;
+        for (int i = startPoint; i < finishPoint; i++) {
+            result += i;
+        }
+        return result;
+    }
+
+    private List<RecursiveTask<Long>> createSubTasks() {
+        List<RecursiveTask<Long>> subTasks = new ArrayList<>();
+        int lengthPoint = finishPoint - startPoint;
+        RecursiveTask<Long> firstTask = new MyTask(startPoint, startPoint + lengthPoint/2 - 1);
+        RecursiveTask<Long> secondTask = new MyTask(startPoint + lengthPoint/2, finishPoint);
+        subTasks.add(firstTask);
+        subTasks.add(secondTask);
+        return subTasks;
+    }
+
 }
