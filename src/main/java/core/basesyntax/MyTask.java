c34d904e877a6f7adfcde3b73d7a07f400fce7a6
djmolot <djmolot@gmail.com>
2022-06-07 12:53:34 +0200

    solved

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,8 +1,11 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
+    private static final int MAX_DISTANCE_NON_STPLITING = 10;
     private int startPoint;
     private int finishPoint;
 
@@ -13,7 +16,34 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        long result = 0;
+        int distance = finishPoint - startPoint;
+        if (Math.abs(distance) > MAX_DISTANCE_NON_STPLITING) {
+            System.out.println("RecursiveTask: Splitting the task by distance: "
+                    + distance + ". " + Thread.currentThread().getName());
+            List<RecursiveTask<Long>> subTasks = createSubTasks();
+            for (RecursiveTask<Long> subTask : subTasks) {
+                subTask.fork();
+            }
+            for (RecursiveTask<Long> subTask : subTasks) {
+                result += subTask.join();
+            }
+        } else {
+            System.out.println("RecursiveTask: Doing the task myself with distance: "
+                    + distance + ". " + Thread.currentThread().getName());
+            for (int i = startPoint; i < finishPoint; i++) {
+                result += i;
+            }
+        }
+        return result;
+    }
+
+    private List<RecursiveTask<Long>> createSubTasks() {
+        List<RecursiveTask<Long>> subTasks = new ArrayList<>();
+        RecursiveTask<Long> first = new MyTask(startPoint, (startPoint + finishPoint) / 2);
+        RecursiveTask<Long> second = new MyTask((startPoint + finishPoint) / 2, finishPoint);
+        subTasks.add(first);
+        subTasks.add(second);
+        return subTasks;
     }
 }
