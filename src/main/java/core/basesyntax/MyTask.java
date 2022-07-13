17fddb643d3d50b4c172f1ab0e83e6464308419a
Maksym Pokras <mxpokras@gmail.com>
2022-07-13 14:48:10 +0200

    MyTask implemented

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,8 +1,12 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.RecursiveTask;
+import java.util.stream.LongStream;
 
 public class MyTask extends RecursiveTask<Long> {
+    private static final int POINT_DIFF_THRESHOLD = 10;
     private int startPoint;
     private int finishPoint;
 
@@ -13,7 +17,26 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        if (finishPoint - startPoint > POINT_DIFF_THRESHOLD) {
+            List<RecursiveTask<Long>> subTasks = createSubTasks(startPoint, finishPoint);
+            for( RecursiveTask<Long> subTask : subTasks) {
+                subTask.fork();
+            }
+            Long result = 0L;
+            for( RecursiveTask<Long> subTask : subTasks) {
+                result += subTask.join();
+            }
+            return result;
+        } else {
+            return LongStream.range(startPoint, finishPoint).sum();
+        }
+    }
+
+    private List<RecursiveTask<Long>> createSubTasks(int startPoint, int finishPoint) {
+        List<RecursiveTask<Long>> subTasks = new ArrayList<>();
+        int midPoint = startPoint + (finishPoint - startPoint) / 2 ;
+        subTasks.add(new MyTask(startPoint, midPoint));
+        subTasks.add(new MyTask(midPoint, finishPoint));
+        return subTasks;
     }
 }
