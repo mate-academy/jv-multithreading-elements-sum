d4a671bce8c6f5260e3a12416656337dbb168afa
yaroslav <morozov.yrv@gmail.com>
2022-08-06 04:32:03 +0300

    calculate summ

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,8 +1,11 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
+    private static final int THRESHOLD = 10;
     private int startPoint;
     private int finishPoint;
 
@@ -13,7 +16,32 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        if (finishPoint - startPoint > THRESHOLD) {
+            List<RecursiveTask> subTasks = createSubTasks();
+            for (RecursiveTask<Long> task : subTasks) {
+                task.fork();
+            }
+            long result = 0;
+            for (RecursiveTask<Long> task : subTasks) {
+                result += task.join();
+            }
+            return result;
+        } else {
+            long result = 0;
+            for (int i = startPoint; i < finishPoint; i++) {
+                result += i;
+            }
+            return result;
+        }
+    }
+
+    private List<RecursiveTask> createSubTasks() {
+        List<RecursiveTask> subTasks = new ArrayList<>();
+        int middlePoint = startPoint + (finishPoint - startPoint) / 2;
+        RecursiveTask first = new MyTask(startPoint, middlePoint);
+        RecursiveTask second = new MyTask(middlePoint, finishPoint);
+        subTasks.add(first);
+        subTasks.add(second);
+        return subTasks;
     }
 }
