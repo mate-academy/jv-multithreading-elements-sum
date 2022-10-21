2eb1287f439523594faa483c97bcf85730f3c7d2
Slava Verbanov <v.verbanov@gmail.com>
2022-10-21 19:19:55 +0200

    create logic in MyTask class

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
@@ -13,7 +15,37 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        int difference = finishPoint - startPoint;
+        if (difference > 10) {
+            System.out.println("RecursiveTask: Splitting task, difference : "
+                    + difference + ". " + Thread.currentThread().getName());
+            List<RecursiveTask<Long>> subTasks = new ArrayList<>(createSubtasks());
+            for (RecursiveTask<Long> subTask : subTasks) {
+                subTask.fork();
+            }
+            long result = 0;
+            for (RecursiveTask<Long> subTask : subTasks) {
+                result += subTask.join();
+            }
+            return result;
+        } else {
+            System.out.println("RecursiveAction: Doing task myself, difference : "
+                    + difference + ". " + Thread.currentThread().getName());
+            long result = 0;
+            for (int i = startPoint; i < finishPoint; i++){
+                result += i;
+            }
+            return result;
+        }
+    }
+
+    private List<RecursiveTask<Long>> createSubtasks() {
+        int difference = (finishPoint - startPoint) / 2;
+        List<RecursiveTask<Long>> subTasks = new ArrayList<>();
+        MyTask first = new MyTask(startPoint, startPoint + difference);
+        MyTask second = new MyTask(startPoint + difference, finishPoint);
+        subTasks.add(first);
+        subTasks.add(second);
+        return subTasks;
     }
 }
