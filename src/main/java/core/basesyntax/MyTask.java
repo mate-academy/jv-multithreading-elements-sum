2aa630b330b6a7fb3d097e970c600c4c9d270086
Vitaliy Zahrenko <vzagorenko@gmail.com>
2022-10-30 21:58:37 +0200

    ForkJoinPool implemented

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
@@ -13,7 +15,45 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        if ((finishPoint - startPoint) <= 1) {
+            return 0L;
+        }
+        if ((finishPoint - startPoint) >= 10) {
+            List<RecursiveTask<Long>> subTasks = new ArrayList<>(createSubtask());
+            for (RecursiveTask<Long> subTask: subTasks) {
+                subTask.fork();
+            }
+            Long result = 0L;
+            for (RecursiveTask<Long> subTask: subTasks) {
+                result += subTask.join();
+            }
+            if (Thread.currentThread().getName().equals("main")) {
+                result = result - finishPoint;
+            }
+            return result;
+        } else {
+            Long result = 0L;
+            int ptr = startPoint;
+            while (ptr <= finishPoint) {
+                result += ptr;
+                ptr++;
+            }
+            if (Thread.currentThread().getName().equals("main")) {
+                result = result - finishPoint;
+            }
+            return result;
+        }
+    }
+
+    private List<RecursiveTask<Long>> createSubtask() {
+        int start = this.startPoint;
+        int end = this.finishPoint;
+        int medium = start + (end - start) / 2;
+        List<RecursiveTask<Long>> subTasks = new ArrayList<>();
+        RecursiveTask<Long> first = new MyTask(start, medium);
+        RecursiveTask<Long> second = new MyTask(medium + 1, end);
+        subTasks.add(first);
+        subTasks.add(second);
+        return subTasks;
     }
 }
