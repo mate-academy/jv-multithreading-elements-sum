d38623372425f040c19a568b0b07962cad27831e
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-11-01 00:51:53 +0200

    implement `compute()` method in class `MyTask`

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
@@ -13,7 +15,30 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        Long result = 0L;
+        if (finishPoint - startPoint > 10) {
+            List<RecursiveTask<Long>> tasks = getTasks();
+            for (RecursiveTask<Long> task : tasks) {
+                task.fork();
+            }
+            for (RecursiveTask<Long> task : tasks) {
+                result += task.join();
+            }
+        } else {
+            for (int i = startPoint; i < finishPoint; i++) {
+                result += i;
+            }
+        }
+        return result;
+    }
+
+    private List<RecursiveTask<Long>> getTasks() {
+        List<RecursiveTask<Long>> tasks = new ArrayList<>();
+        int middle = startPoint + (finishPoint - startPoint) / 2;
+        RecursiveTask<Long> taskOne = new MyTask(startPoint, middle);
+        RecursiveTask<Long> taskTwo = new MyTask(middle, finishPoint);
+        tasks.add(taskOne);
+        tasks.add(taskTwo);
+        return tasks;
     }
 }
