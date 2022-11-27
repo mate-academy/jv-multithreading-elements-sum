6d762905010400b1d37afef863f37222958e9667
Valentyn Slutskyi <vaaalik45@gmail.com>
2022-11-27 14:39:18 +0200

    Was added solution

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,10 +1,12 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
-    private int startPoint;
-    private int finishPoint;
+    private final int startPoint;
+    private final int finishPoint;
 
     public MyTask(int startPoint, int finishPoint) {
         this.startPoint = startPoint;
@@ -13,7 +15,32 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        if (finishPoint - startPoint > 10) {
+            List<RecursiveTask<Long>> subIntervals = new ArrayList<>(createSubInterval());
+            for (RecursiveTask<Long> subInterval : subIntervals) {
+                subInterval.fork();
+            }
+            long result = 0;
+            for (RecursiveTask<Long> subInterval : subIntervals) {
+                result += subInterval.join();
+            }
+            return result;
+        } else {
+            long sum = 0;
+            for (long i = startPoint; i < finishPoint; i++) {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+
+    private List<RecursiveTask<Long>> createSubInterval() {
+        List<RecursiveTask<Long>> subIntervals = new ArrayList<>();
+        int interval = (finishPoint - startPoint) / 2;
+        RecursiveTask<Long> first = new MyTask(startPoint, finishPoint - interval);
+        RecursiveTask<Long> second = new MyTask(finishPoint - interval, finishPoint);
+        subIntervals.add(first);
+        subIntervals.add(second);
+        return subIntervals;
     }
 }
