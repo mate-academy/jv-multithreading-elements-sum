91b74e65c364ed7ea31d2de70fb3856fba83e993
AntonPav <streetreadger@gmail.com>
2022-12-21 23:35:29 +0200

    Solve the task

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
@@ -13,7 +15,32 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        long result = 0;
+        int distance = finishPoint - startPoint;
+        if (distance > 10) {
+            List<RecursiveTask<Long>> recursiveTasks = new ArrayList<>(createSubTask());
+            for (RecursiveTask<Long> task : recursiveTasks) {
+                task.fork();
+                result += task.join();
+            }
+        } else {
+            int value = startPoint;
+            int counter = distance;
+            while (counter > 0) {
+                result += value++;
+                counter--;
+            }
+        }
+        return result;
+    }
+
+    private List<RecursiveTask<Long>> createSubTask() {
+        List<RecursiveTask<Long>> subTasks = new ArrayList<>();
+        int nextStartPoint = startPoint + 9;
+        RecursiveTask<Long> first = new MyTask(startPoint, nextStartPoint);
+        RecursiveTask<Long> second = new MyTask(nextStartPoint, finishPoint);
+        subTasks.add(first);
+        subTasks.add(second);
+        return subTasks;
     }
 }
