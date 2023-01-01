55b691951ca620d6531906dc516f8056b855c47c
Anastasiia Romanova <ana.roma.vi@gmail.com>
2023-01-01 21:19:16 +0200

    implemented compute() method

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,13 +1,14 @@
 package core.basesyntax;
 
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
 
 public class Main {
     public static void main(String[] args) {
         ForkJoinPool forkJoinPool = ForkJoinPool.commonPool();
 
-        MyTask task = new MyTask(0, 100);
-        Long result = forkJoinPool.invoke(task);
+        RecursiveTask<Long> myTask = new MyTask(0, 100);
+        Long result = forkJoinPool.invoke(myTask);
         System.out.println("Result = " + result);
     }
 }
