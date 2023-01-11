1b3b03f35721d5d15eda78150801bd50b270f0de
Danyl Kniaziev <kdanil0401@gmail.com>
2023-01-11 16:09:10 +0200

    done all task

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,8 +1,11 @@
 package core.basesyntax;
 
+import java.util.List;
 import java.util.concurrent.RecursiveTask;
+import java.util.stream.LongStream;
 
 public class MyTask extends RecursiveTask<Long> {
+    private final static int COUNT_TASKS_FOR_FORK = 10;
     private int startPoint;
     private int finishPoint;
 
@@ -14,6 +17,23 @@ public class MyTask extends RecursiveTask<Long> {
     @Override
     protected Long compute() {
         // write your code here
-        return null;
+        long sum = 0;
+        if (finishPoint - startPoint > COUNT_TASKS_FOR_FORK) {
+            for (RecursiveTask<Long> task : creatSubTask()) {
+                task.fork();
+                sum += task.join();
+            }
+            return sum;
+        } else {
+            return LongStream.range(startPoint, finishPoint).sum();
+        }
+    }
+
+    private List<RecursiveTask<Long>> creatSubTask() {
+        int forkPoint = startPoint + (finishPoint - startPoint) / 2;
+        return List.of(
+                new MyTask(startPoint, forkPoint),
+                new MyTask(forkPoint, finishPoint)
+        );
     }
 }
