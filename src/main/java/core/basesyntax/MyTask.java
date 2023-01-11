fba0118cc8abec6419c94bc7e14d320e8838faf8
Andrii Matveev <matveev.tesla@gmail.com>
2023-01-11 13:00:26 +0200

    homework has been done

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,6 +1,9 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.RecursiveTask;
+import java.util.stream.LongStream;
 
 public class MyTask extends RecursiveTask<Long> {
     private int startPoint;
@@ -13,7 +16,29 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        long result = 0;
+        if (Math.abs(finishPoint) - Math.abs(startPoint) > 10) {
+            List<RecursiveTask<Long>> recursiveTasks = new ArrayList<>(createSubTask());
+            for (RecursiveTask<Long> subTasks : recursiveTasks) {
+                subTasks.fork();
+            }
+            for (RecursiveTask<Long> subTasks : recursiveTasks) {
+                result += subTasks.join();
+            }
+        } else {
+            result = LongStream.range(startPoint, finishPoint)
+                    .sum();
+        }
+        return result;
+    }
+
+    private List<RecursiveTask<Long>> createSubTask() {
+        List<RecursiveTask<Long>> subTasks = new ArrayList<>();
+        RecursiveTask<Long> first = new MyTask(startPoint, startPoint + 9);
+        RecursiveTask<Long> second = new MyTask(startPoint + 9, finishPoint);
+        System.out.println("current startpoint = " + startPoint);
+        subTasks.add(first);
+        subTasks.add(second);
+        return subTasks;
     }
 }
