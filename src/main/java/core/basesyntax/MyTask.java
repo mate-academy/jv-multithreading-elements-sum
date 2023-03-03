9b88be0b50226e527418db5ded3205e782fee62f
FishinMaksim <max.freedman.main@gmail.com>
2023-03-04 01:58:56 +0200

    Added solution

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,8 +1,13 @@
 package core.basesyntax;
 
+import java.util.List;
+import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.RecursiveTask;
+import java.util.stream.LongStream;
 
 public class MyTask extends RecursiveTask<Long> {
+    private static final int THRESHOLD = 10;
+
     private int startPoint;
     private int finishPoint;
 
@@ -13,7 +18,29 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        if (isNeedToSplitBySubtasks()) {
+            return ForkJoinTask.invokeAll(createSubtasks())
+                    .stream()
+                    .mapToLong(ForkJoinTask::join)
+                    .sum();
+        }
+
+        return LongStream.range(startPoint, finishPoint)
+                .sum();
     }
+
+    private List<RecursiveTask<Long>> createSubtasks() {
+        int middlePoint = getMiddlePoint();
+
+        return List.of(new MyTask(startPoint, middlePoint), new MyTask(middlePoint, finishPoint));
+    }
+
+    private boolean isNeedToSplitBySubtasks() {
+        return Math.abs(startPoint - finishPoint) > THRESHOLD;
+    }
+
+    private int getMiddlePoint() {
+        return (startPoint + finishPoint) >> 1;
+    }
+
 }
