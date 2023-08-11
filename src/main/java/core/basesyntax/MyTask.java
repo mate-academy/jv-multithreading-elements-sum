4afb4019d8d8cc7d27d41bda49f3590f6faac4d3
Pavlo Krasota <pavlokrasota@gmail.com>
2023-08-11 09:50:27 +0300

    Solved solution with using forkJoinPoll :)

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,8 +1,11 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
+    private static final int THRESHOLD = 10;
     private int startPoint;
     private int finishPoint;
 
@@ -13,7 +16,26 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        long result = 0;
+        if (finishPoint - startPoint > THRESHOLD) {
+            List<MyTask> subTask = createSubTask();
+            invokeAll(subTask);
+            for (MyTask task : subTask) {
+                result += task.join();
+            }
+        } else {
+            for (int i = startPoint; i < finishPoint; i++) {
+                result += i;
+            }
+        }
+        return result;
+    }
+
+    private List<MyTask> createSubTask() {
+        List<MyTask> subTask = new ArrayList<>();
+        int middle = (startPoint + finishPoint) / 2;
+        subTask.add(new MyTask(startPoint, middle));
+        subTask.add(new MyTask(middle, finishPoint));
+        return subTask;
     }
 }
