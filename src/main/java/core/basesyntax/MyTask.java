c132b4878c7bf6f17ade859af561cffd37d3e93a
Vladyslav Fesenko <vlad.11.fesenko@gmail.com>
2023-10-07 14:44:28 +0300

    solution created

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
@@ -14,6 +16,41 @@ public class MyTask extends RecursiveTask<Long> {
     @Override
     protected Long compute() {
         // write your code here
-        return null;
+        if (getCurrentDistance() > 10) {
+            List<RecursiveTask<Long>> subTasks = createSubTasks();
+            for (RecursiveTask<Long> subTask : subTasks) {
+                subTask.fork();
+            }
+            long result = 0;
+            for (RecursiveTask<Long> subTask : subTasks) {
+                result += subTask.join();
+            }
+            return result;
+        } else {
+            return getConsecutiveSum(startPoint, finishPoint);
+        }
+    }
+
+    private List<RecursiveTask<Long>> createSubTasks() {
+        List<RecursiveTask<Long>> subTasks = new ArrayList<>();
+        RecursiveTask<Long> firstTask = new MyTask(
+                startPoint, (int) (startPoint + getCurrentDistance() / 2));
+        RecursiveTask<Long> secondTask = new MyTask(
+                (int) (startPoint + getCurrentDistance() / 2), finishPoint);
+        subTasks.add(firstTask);
+        subTasks.add(secondTask);
+        return subTasks;
+    }
+
+    private Long getCurrentDistance() {
+        return (long) (finishPoint - startPoint);
+    }
+
+    private long getConsecutiveSum(long from, long to) {
+        long sum = 0;
+        for (long i = from; i < to; i++) {
+            sum += i;
+        }
+        return sum;
     }
 }
