ead64ef112752c599e5bc450137fce6ee0392305
Zhdanov Anton <anton1992xxxx@gmail.com>
2023-10-12 01:08:16 +0300

    hw-multithreading-elements-sum

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
-    private int startPoint;
-    private int finishPoint;
+    private static final int THRESHOLD = 10;
+    private final int startPoint;
+    private final int finishPoint;
 
     public MyTask(int startPoint, int finishPoint) {
         this.startPoint = startPoint;
@@ -13,7 +14,24 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        if (startPoint >= finishPoint) {
+            return 0L;
+        }
+        if (finishPoint - startPoint <= THRESHOLD) {
+            return sumOfConsecutiveIntegers(startPoint, finishPoint);
+
+        } else {
+            int mid = (startPoint + finishPoint) / 2;
+            MyTask leftTask = new MyTask(startPoint, mid);
+            MyTask rightTask = new MyTask(mid, finishPoint);
+            leftTask.fork();
+            long rightResult = rightTask.compute();
+            long leftResult = leftTask.join();
+            return leftResult + rightResult;
+        }
+    }
+
+    private long sumOfConsecutiveIntegers(int start, int end) {
+        return (long) (end - start) * (start + end - 1) / 2;
     }
 }
