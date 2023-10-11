56e5ac3e2d7cf43bc626b08eba9fd5a3d7c7dac3
Mykhailo Kuryk <113907200+mishaa16@users.noreply.github.com>
2023-10-11 18:24:58 +0300

    create a solution

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -13,7 +13,24 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        if (finishPoint - startPoint <= 10) {
+            long sum = 0;
+            for (int i = startPoint; i < finishPoint; i++) {
+                sum += i;
+            }
+            System.out.println("Sum from " + startPoint + " to " + finishPoint + " is " + sum);
+            return sum;
+        } else {
+            int mid = (startPoint + finishPoint) / 2;
+            MyTask leftTask = new MyTask(startPoint, mid);
+            MyTask rightTask = new MyTask(mid, finishPoint);
+
+            leftTask.fork();
+            rightTask.fork();
+
+            Long leftResult = leftTask.join();
+            Long rightResult = rightTask.join();
+            return leftResult + rightResult;
+        }
     }
 }
