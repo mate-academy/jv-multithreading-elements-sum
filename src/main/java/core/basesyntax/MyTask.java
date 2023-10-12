7706ff97bc8ecbae3f0ed56947f8ef49fa81e060
Denis Balako <denisarius2000s@gmail.com>
2023-10-12 17:24:29 +0300

    improve code

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -8,15 +8,17 @@ import java.util.stream.IntStream;
 public class MyTask extends RecursiveTask<Long> {
     private final int startPoint;
     private final int finishPoint;
+    private final int currentRange;
 
     public MyTask(int startPoint, int finishPoint) {
         this.startPoint = startPoint;
         this.finishPoint = finishPoint;
+        this.currentRange = finishPoint - startPoint;
     }
 
     @Override
     protected Long compute() {
-        if (getCurrentRange() > 10) {
+        if (currentRange > 10) {
             List<RecursiveTask<Long>> recursiveTasks = new ArrayList<>(createSubTasks());
             for (RecursiveTask<Long> subTask : recursiveTasks) {
                 subTask.fork();
@@ -27,21 +29,19 @@ public class MyTask extends RecursiveTask<Long> {
             }
             return result;
         } else {
-            return (long) IntStream.range(startPoint, finishPoint).sum();
+            return IntStream.range(startPoint, finishPoint)
+                    .asLongStream()
+                    .sum();
         }
     }
 
     private List<RecursiveTask<Long>> createSubTasks() {
         List<RecursiveTask<Long>> subTasks = new ArrayList<>();
-        int middlePoint = (getCurrentRange() / 2) + startPoint;
+        int middlePoint = (currentRange / 2) + startPoint;
         RecursiveTask<Long> first = new MyTask(startPoint, middlePoint);
         RecursiveTask<Long> second = new MyTask(middlePoint, finishPoint);
         subTasks.add(first);
         subTasks.add(second);
         return subTasks;
-    }
-
-    private int getCurrentRange() {
-        return finishPoint - startPoint;
     }
 }
