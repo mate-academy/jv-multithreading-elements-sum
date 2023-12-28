ecef0536acc5d7e7624b6bd7a3415a3494f9682c
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2023-12-28 14:28:20 +0200

    solved mvn checkstyle mistakes

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -36,8 +36,10 @@ public class MyTask extends RecursiveTask<Long> {
 
     private List<RecursiveTask<Long>> createSubTasks() {
         List<RecursiveTask<Long>> subTasks = new ArrayList<>();
-        RecursiveTask<Long> first = new MyTask(startPoint, startPoint + (finishPoint - startPoint) / 2);
-        RecursiveTask<Long> second = new MyTask(startPoint + (finishPoint - startPoint) / 2, finishPoint);
+        RecursiveTask<Long> first = new MyTask(startPoint,
+                startPoint + (finishPoint - startPoint) / 2);
+        RecursiveTask<Long> second = new MyTask(startPoint +
+                (finishPoint - startPoint) / 2, finishPoint);
         subTasks.add(first);
         subTasks.add(second);
         return subTasks;
