0515c2b164da69ed195a7703d17636f741b4d0df
Dima Semeniuk <semeniuk.dima@ukr.net>
2024-01-26 15:05:31 +0200

    comleted task Sum finding ussing RecursiveTask class

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,8 +1,10 @@
 package core.basesyntax;
 
 import java.util.concurrent.RecursiveTask;
+import java.util.stream.LongStream;
 
 public class MyTask extends RecursiveTask<Long> {
+    private static final int THRESHOLD = 10;
     private int startPoint;
     private int finishPoint;
 
@@ -13,7 +15,22 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        if (finishPoint - startPoint <= THRESHOLD) {
+            return sum();
+        }
+        int middlePoint = startPoint + (finishPoint - startPoint) / 2;
+        MyTask leftTask = new MyTask(startPoint, middlePoint);
+        MyTask rightTask = new MyTask(middlePoint, finishPoint);
+
+        leftTask.fork();
+        rightTask.fork();
+        Long leftResult = leftTask.join();
+        Long rightResult = rightTask.join();
+        return Long.sum(leftResult, rightResult);
+    }
+
+    private Long sum() {
+        return LongStream.range(startPoint, finishPoint)
+                .sum();
     }
 }
