63dce332b341743120f6e3f17579ff0a10b9e85c
Volodymyr Ozemblovskyi <Vladimir.ozem.ps@gmail.com>
2024-01-27 19:15:57 +0200

    solve task from readme file

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -4,8 +4,8 @@ import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
     private static final int THRESHOLD = 10;
-    private int startPoint;
-    private int finishPoint;
+    private final int startPoint;
+    private final int finishPoint;
 
     public MyTask(int startPoint, int finishPoint) {
         this.startPoint = startPoint;
@@ -24,13 +24,10 @@ public class MyTask extends RecursiveTask<Long> {
             int middlePoint = startPoint + (finishPoint - startPoint) / 2;
             MyTask leftTask = new MyTask(startPoint, middlePoint);
             MyTask rightTask = new MyTask(middlePoint, finishPoint);
-
             leftTask.fork();
             rightTask.fork();
-
             Long rightResult = rightTask.join();
             Long leftResult = leftTask.join();
-
             return rightResult + leftResult;
         }
     }
