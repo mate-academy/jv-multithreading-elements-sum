701aacf92d39310b8d5d5e89e6b18bd5ed392c74
Nataliia Shtykh <forstudying93@gmail.com>
2024-01-28 17:11:59 +0200

    implemented compute() method

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,10 +1,12 @@
 package core.basesyntax;
 
+import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
     private int startPoint;
     private int finishPoint;
+    private int distance = finishPoint - startPoint;
 
     public MyTask(int startPoint, int finishPoint) {
         this.startPoint = startPoint;
@@ -13,7 +15,23 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        if (distance > 10) {
+            List<RecursiveTask<Long>> subTasks = createSubTasks();
+            for (RecursiveTask<Long> subTask : subTasks) {
+                subTask.fork();
+            }
+        }
+        long sum = 0L;
+        for (int i = startPoint; i < finishPoint; i++) {
+            sum += i;
+        }
+        return sum;
+
+    }
+
+    private List<RecursiveTask<Long>> createSubTasks() {
+        RecursiveTask<Long> first = new MyTask(startPoint, finishPoint / 2);
+        RecursiveTask<Long> second = new MyTask(finishPoint / 2 + 1, finishPoint);
+        return List.of(first, second);
     }
 }
