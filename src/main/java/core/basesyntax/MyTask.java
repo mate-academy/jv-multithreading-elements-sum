21c102d55b4a08b710980a0b35b6e223bc3b17d2
Krasnov-Maksim <maksim.v.krasnov@gmail.com>
2024-04-19 13:01:44 +0300

    solved jv-multithreading-elements-sum

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
-    private int startPoint;
-    private int finishPoint;
+    private static final int THRESHOLD = 10;
+    private final int startPoint;
+    private final int finishPoint;
 
     public MyTask(int startPoint, int finishPoint) {
         this.startPoint = startPoint;
@@ -13,7 +14,30 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        if (finishPoint - startPoint < 0) {
+            return 0L;
+        } else if (finishPoint - startPoint <= THRESHOLD) {
+            return getSum(startPoint, finishPoint);
+        } else {
+            int mid = startPoint + (finishPoint - startPoint) / 2 - 1;
+            MyTask leftTask = new MyTask(startPoint, mid);
+            MyTask rightTask = new MyTask(mid, finishPoint);
+
+            leftTask.fork();
+            rightTask.fork();
+
+            long rightResult = rightTask.compute();
+            long leftResult = leftTask.join();
+
+            return leftResult + rightResult;
+        }
+    }
+
+    private long getSum(int from, int to) {
+        long sum = 0;
+        for (int i = from; i < to; i++) {
+            sum += i;
+        }
+        return sum;
     }
 }
