373d5cf606c53f49f28d06bf9eeaa6d4e308b6fe
vbilodon <vovabilodon@gmail.com>
2024-04-21 23:24:28 +0300

    implemented MyTask

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -6,6 +6,8 @@ public class MyTask extends RecursiveTask<Long> {
     private int startPoint;
     private int finishPoint;
 
+    private static final int THRESHOLD = 10;
+
     public MyTask(int startPoint, int finishPoint) {
         this.startPoint = startPoint;
         this.finishPoint = finishPoint;
@@ -13,7 +15,24 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        if (finishPoint - startPoint > THRESHOLD) {
+            int midPoint = startPoint + (finishPoint - startPoint) / 2;
+            var left = new MyTask(startPoint, midPoint);
+            var right = new MyTask(midPoint, finishPoint);
+
+            left.fork();
+            right.fork();
+
+            long leftResult = left.join();
+            long rightResult = right.join();
+
+            return leftResult + rightResult;
+        } else {
+            var result = 0;
+            for (int i = startPoint; i < finishPoint; i++) {
+                result += i;
+            }
+            return (long) result;
+        }
     }
 }
