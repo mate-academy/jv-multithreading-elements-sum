15066ac23aeef4302888403c98af3c5c2f3f5e67
Eduard Fakhrutdinov <edikf03@gmail.com>
2024-06-17 16:05:29 +0300

    feat: 1 relize compute

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -3,6 +3,7 @@ package core.basesyntax;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
+    private static final int THRESHOLD = 10;
     private int startPoint;
     private int finishPoint;
 
@@ -13,7 +14,20 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        if (finishPoint - startPoint <= THRESHOLD) {
+            long sum = 0;
+            for (int i = startPoint; i < finishPoint; ++i) {
+                sum += i;
+            }
+            return sum;
+        } else {
+            int midPoint = startPoint + (finishPoint - startPoint) / 2;
+            MyTask firstPart = new MyTask(startPoint, midPoint);
+            MyTask secondPart = new MyTask(midPoint, finishPoint);
+            firstPart.fork();
+            Long res2 = secondPart.compute();
+            Long res1 = firstPart.join();
+            return res1 + res2;
+        }
     }
 }
