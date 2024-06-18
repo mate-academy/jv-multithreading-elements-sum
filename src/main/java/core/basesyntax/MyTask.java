974840e554357e63e17c0ac0bf171598883b0f00
VolandevlodD <vladradko2000@gmail.com>
2024-06-18 19:45:26 +0300

    Update MyTask.java

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,10 +1,12 @@
 package core.basesyntax;
 
 import java.util.concurrent.RecursiveTask;
+import java.util.stream.LongStream;
 
 public class MyTask extends RecursiveTask<Long> {
-    private int startPoint;
-    private int finishPoint;
+    private final int startPoint;
+    private final int finishPoint;
+    private static final int THRESHOLD = 10;
 
     public MyTask(int startPoint, int finishPoint) {
         this.startPoint = startPoint;
@@ -13,7 +15,15 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        if (finishPoint - startPoint <= THRESHOLD) {
+            return LongStream.range(startPoint, finishPoint).sum();
+        } else {
+            int middle = startPoint + (finishPoint-startPoint) / 2;
+            MyTask left = new MyTask(startPoint, middle);
+            MyTask right = new MyTask(middle, finishPoint);
+            left.fork();
+            right.fork();
+            return left.join() + right.join();
+        }
     }
 }
