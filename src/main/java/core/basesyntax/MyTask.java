b78bafc167dbdc9337d113fc899c490ae99c80a3
makaroshyna <katya23225@gmail.com>
2024-06-18 19:03:10 +0300

    Implement compute().

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,8 +1,10 @@
 package core.basesyntax;
 
 import java.util.concurrent.RecursiveTask;
+import java.util.stream.LongStream;
 
 public class MyTask extends RecursiveTask<Long> {
+    private static final int THRESHOLD = 10;
     private int startPoint;
     private int finishPoint;
 
@@ -13,7 +15,16 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        if (finishPoint - startPoint <= 10) {
+            return LongStream.range(startPoint, finishPoint).sum();
+        } else {
+            int middle = startPoint + (finishPoint - startPoint) / 2;
+            MyTask left = new MyTask(startPoint, middle);
+            MyTask right = new MyTask(middle, finishPoint);
+
+            left.fork();
+            right.fork();
+            return left.join() + right.join();
+        }
     }
 }
