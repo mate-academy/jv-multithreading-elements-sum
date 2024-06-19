818d9cb256d6b78657cb0e3ef8440230b30942e0
Valentyn Sharshon <woosay333@gmail.com>
2024-06-19 19:35:04 +0300

    Elements sum implementation

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,10 +1,14 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
-    private int startPoint;
-    private int finishPoint;
+    private static final long MAX_DISTANCE = 10;
+
+    private final int startPoint;
+    private final int finishPoint;
 
     public MyTask(int startPoint, int finishPoint) {
         this.startPoint = startPoint;
@@ -13,7 +17,30 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        long result = 0;
+
+        if (finishPoint - startPoint > MAX_DISTANCE) {
+            List<RecursiveTask<Long>> subTasks = createSubTasksList();
+            for (RecursiveTask<Long> subTask : subTasks) {
+                subTask.fork();
+            }
+            for (RecursiveTask<Long> subTask : subTasks) {
+                result += subTask.join();
+            }
+        } else {
+            for (int i = startPoint; i < finishPoint; i++) {
+                result += i;
+            }
+        }
+
+        return result;
+    }
+
+    private List<RecursiveTask<Long>> createSubTasksList() {
+        List<RecursiveTask<Long>> subTasks = new ArrayList<>();
+        int midPoint = (startPoint + finishPoint) / 2;
+        subTasks.add(new MyTask(startPoint, midPoint));
+        subTasks.add(new MyTask(midPoint, finishPoint));
+        return subTasks;
     }
 }
