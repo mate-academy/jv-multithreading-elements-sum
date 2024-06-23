ff2f1cc28cbeacbd4c7c264bd01ebb7ce76746a3
Nazar Klimovych <rksvjdbx@gmail.com>
2024-06-23 17:17:34 +0300

    Added implementation of method compute() in MyTask class.

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,8 +1,12 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
+    private static final int MAX_DIFFERENCE = 10;
+
     private int startPoint;
     private int finishPoint;
 
@@ -13,7 +17,41 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        int workLoad = finishPoint - startPoint;
+
+        if (workLoad > MAX_DIFFERENCE) {
+            List<MyTask> subTasks = createSubTasks();
+
+            for (MyTask subTask : subTasks) {
+                subTask.fork();
+            }
+
+            long result = 0;
+            for (MyTask subTask : subTasks) {
+                result += subTask.join();
+            }
+            return result;
+        } else {
+            return sum(startPoint, finishPoint);
+        }
+    }
+
+    private List<MyTask> createSubTasks() {
+        List<MyTask> subTasks = new ArrayList<>();
+        int middle = startPoint + (finishPoint - startPoint) / 2;
+
+        MyTask first = new MyTask(startPoint, middle);
+        MyTask second = new MyTask(middle, finishPoint);
+        subTasks.add(first);
+        subTasks.add(second);
+        return subTasks;
+    }
+
+    private long sum(int start, int end) {
+        long sum = 0;
+        for (int i = start; i < end; i++) {
+            sum += i;
+        }
+        return sum;
     }
 }
