216c7a258904421c29c259acec5ef0c3dd79ea92
Artem Klishch <artemklishch@gmail.com>
2024-10-22 21:56:22 +0300

    feat: made logic to calculate the sume of numbers

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -3,7 +3,10 @@ package core.basesyntax;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
+    private static final int THRESHOLD = 10;
+
     private int startPoint;
+
     private int finishPoint;
 
     public MyTask(int startPoint, int finishPoint) {
@@ -13,7 +16,30 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        if (finishPoint - startPoint <= THRESHOLD) {
+            return getSum(startPoint, finishPoint);
+        } else {
+            int middle = startPoint + (finishPoint - startPoint) / 2;
+            MyTask left = new MyTask(startPoint, middle);
+            MyTask right = new MyTask(middle, finishPoint);
+
+            left.fork();
+            right.fork();
+
+            long rightResult = left.join();
+            long leftResult = right.join();
+            return Long.sum(leftResult, rightResult);
+        }
+    }
+
+    private long getSum(long start, long end) {
+        if (start == end || start > end) {
+            return 0;
+        }
+        long result = start;
+        for (long i = start + 1; i < end; i++) {
+            result += i;
+        }
+        return result;
     }
 }
