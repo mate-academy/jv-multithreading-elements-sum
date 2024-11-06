af438f1564a721368277ddfa0b2081df3824b49c
Yevhen Shumeiko <shymeiko.zenya@gmail.com>
2024-11-06 20:56:26 +0200

    solved

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -3,6 +3,7 @@ package core.basesyntax;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
+    public static final int THRESHOLD = 10;
     private int startPoint;
     private int finishPoint;
 
@@ -13,7 +14,28 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        if (finishPoint - startPoint <= THRESHOLD) {
+            return findSumSequentially(startPoint, finishPoint);
+        } else {
+            int middle = (finishPoint + startPoint) / 2;
+            MyTask leftTask = new MyTask(startPoint, middle);
+            MyTask rightTask = new MyTask(middle, finishPoint);
+
+            leftTask.fork();
+            rightTask.fork();
+
+            Long leftResult = leftTask.join();
+            Long rightResult = rightTask.join();
+
+            return leftResult + rightResult;
+        }
+    }
+
+    private Long findSumSequentially(long startPoint, long finishPoint) {
+        Long sum = 0L;
+        for (long i = startPoint; i < finishPoint; i++) {
+            sum += i;
+        }
+        return sum;
     }
 }
