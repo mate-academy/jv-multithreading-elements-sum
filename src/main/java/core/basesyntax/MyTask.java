0d4bab593ebf9421845ef6f5f56a70eb797c3adb
OLEH <avanesovob@gmail.com>
2024-11-08 14:05:05 +0200

    Added solution

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -13,7 +13,27 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        if (finishPoint - startPoint <= 10) {
+            return sum(startPoint, finishPoint);
+        } else {
+            int middlePoint = startPoint + (finishPoint - startPoint) / 2;
+            MyTask leftTask = new MyTask(startPoint, middlePoint);
+            MyTask rightTask = new MyTask(middlePoint, finishPoint);
+
+            leftTask.fork();
+            rightTask.fork();
+
+            Long leftResult = leftTask.join();
+            Long rightResult = rightTask.join();
+            return Long.sum(leftResult, rightResult);
+        }
+    }
+
+    private Long sum(int startPoint, int finishPoint) {
+        int sum = 0;
+        for (int i = startPoint; i < finishPoint; i++) {
+            sum += i;
+        }
+        return (long) sum;
     }
 }
