00b0e485d0cbdba8bf4867fad6b02b8973393be5
bobik <oliferchukrivne@i.ua>
2024-11-09 21:53:40 +0200

    done

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
-    private int startPoint;
-    private int finishPoint;
+    private static final int THRESHOLD = 10;
+    private final int startPoint;
+    private final int finishPoint;
 
     public MyTask(int startPoint, int finishPoint) {
         this.startPoint = startPoint;
@@ -13,7 +14,27 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        if (finishPoint - startPoint <= THRESHOLD) {
+            return findSum(startPoint, finishPoint);
+        } else {
+            int middle = startPoint + (finishPoint - startPoint) / 2;
+            MyTask leftSum = new MyTask(startPoint, middle);
+            MyTask rightSum = new MyTask(middle, finishPoint);
+
+            leftSum.fork();
+            rightSum.fork();
+            int rightResult = Math.toIntExact(rightSum.join());
+            int leftResult = Math.toIntExact(leftSum.join());
+
+            return (long) Math.addExact(rightResult, leftResult);
+        }
+    }
+
+    private long findSum(int startPoint, int finishPoint) {
+        long sum = 0;
+        for (int i = startPoint; i < finishPoint; i++) {
+            sum += i;
+        }
+        return sum;
     }
 }
