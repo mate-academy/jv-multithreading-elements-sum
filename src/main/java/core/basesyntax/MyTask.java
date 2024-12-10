dcbc2db4919581d71902315652d680b6a95b707a
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-12-10 12:00:56 +0200

    sum the numbers

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,8 +1,10 @@
 package core.basesyntax;
 
 import java.util.concurrent.RecursiveTask;
+import java.util.stream.LongStream;
 
 public class MyTask extends RecursiveTask<Long> {
+    private static final int THRESHOLD = 10;
     private int startPoint;
     private int finishPoint;
 
@@ -13,7 +15,24 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        if (finishPoint - startPoint <= THRESHOLD) {
+            return sumTheNumbers(startPoint, finishPoint);
+        } else {
+            int middlePoint = startPoint + (finishPoint - startPoint) / 2;
+            MyTask left = new MyTask(startPoint, middlePoint);
+            MyTask right = new MyTask(middlePoint, finishPoint);
+            left.fork();
+            right.fork();
+
+            Long leftResult = left.join();
+            Long rightResult = right.join();
+
+            return leftResult + rightResult;
+        }
+    }
+
+    private Long sumTheNumbers(int startPoint, int finishPoint) {
+        return LongStream.range(startPoint, finishPoint)
+                .sum();
     }
 }
