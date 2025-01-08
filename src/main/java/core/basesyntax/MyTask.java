1111de97ebfb71419f3e37025eb1f2daf3c782dd
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-01-08 15:32:30 +0200

    The logic of the compute method is implemented.

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
@@ -13,7 +15,29 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        Long result = 0L;
+        if (finishPoint - startPoint > 10) {
+            List<RecursiveTask<Long>> subTasks = new ArrayList<>(createSubTask());
+            for (RecursiveTask<Long> subTask : subTasks) {
+                subTask.fork();
+            }
+            for (RecursiveTask<Long> subTask : subTasks) {
+                result += subTask.join();
+            }
+        } else {
+            for (int i = startPoint; i < finishPoint; i++) {
+                result += i;
+            }
+        }
+        return result;
+    }
+
+    private List<RecursiveTask<Long>> createSubTask() {
+        List<RecursiveTask<Long>> subTasks = new ArrayList<>();
+        RecursiveTask<Long> firstTask = new MyTask(startPoint, startPoint + 10);
+        RecursiveTask<Long> secondTask = new MyTask(startPoint + 10, finishPoint);
+        subTasks.add(firstTask);
+        subTasks.add(secondTask);
+        return subTasks;
     }
 }
