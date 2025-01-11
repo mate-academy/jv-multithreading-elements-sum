c4f52904051cf74480b2831ac36c77fbadc137b9
Vlad <mazafackery@gmail.com>
2025-01-11 15:31:17 +0200

    complete

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -3,6 +3,7 @@ package core.basesyntax;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
+    public static final int THRESHOLD = 10;
     private int startPoint;
     private int finishPoint;
 
@@ -13,7 +14,32 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        if (finishPoint - startPoint <= THRESHOLD) {
+            System.out.printf("Doing sum of number. Thread: %s%n",
+                    Thread.currentThread().getName());
+
+            return sum(startPoint, finishPoint);
+        } else {
+            int middle = startPoint + (finishPoint - startPoint) / 2;
+
+            MyTask leftTask = new MyTask(startPoint, middle);
+            MyTask rightTask = new MyTask(middle, finishPoint);
+
+            leftTask.fork();
+            rightTask.fork();
+
+            long leftResult = leftTask.join();
+            long rightResult = rightTask.join();
+
+            return leftResult + rightResult;
+        }
+    }
+
+    private Long sum(int start, int end) {
+        long total = 0;
+        for (int i = start; i < end; i++) {
+            total += i;
+        }
+        return total;
     }
 }
