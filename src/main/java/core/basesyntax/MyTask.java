6e711814c9f11368b14410f5f57716d4ae14e044
LeonIllya <dd0733567@gmail.com>
2025-01-17 19:48:57 +0200

    my decision

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -13,7 +13,22 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        if (finishPoint - startPoint < 10) {
+            long sum = 0;
+            for (int i = startPoint; i < finishPoint; i++) {
+                sum += i;
+            }
+            return sum;
+        } else {
+            MyTask leftTask = new MyTask(startPoint, (startPoint + finishPoint) / 2);
+            MyTask rightTask = new MyTask((startPoint + finishPoint) / 2, finishPoint);
+
+            leftTask.fork();
+            rightTask.fork();
+
+            long leftResult = leftTask.join();
+            long rightResult = rightTask.join();
+            return leftResult + rightResult;
+        }
     }
 }
