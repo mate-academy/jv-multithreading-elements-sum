132be60fd63bd79a3da45c2328d48deff8c70a6c
Vladyslav Supruniuk <Vladyslav.Supruniuk@contracted.pmi.com>
2025-01-22 23:34:44 +0200

    Solution

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,10 +1,12 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
-    private int startPoint;
-    private int finishPoint;
+    private final int startPoint;
+    private final int finishPoint;
 
     public MyTask(int startPoint, int finishPoint) {
         this.startPoint = startPoint;
@@ -13,7 +15,35 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        if (finishPoint - startPoint >= 10) {
+            List<RecursiveTask<Long>> subTasks = new ArrayList<>(createSubTasks());
+
+            for (RecursiveTask<Long> task : subTasks) {
+                task.fork();
+            }
+
+            Long result = 0L;
+
+            for (RecursiveTask<Long> task : subTasks) {
+                result += task.join();
+            }
+
+            return result;
+        } else {
+            long result = 0L;
+
+            for (int i = startPoint; i < finishPoint; i++) {
+                result += i;
+            }
+
+            return result;
+        }
+    }
+
+    private List<RecursiveTask<Long>> createSubTasks() {
+        return List.of(
+                new MyTask(startPoint, (startPoint + finishPoint) / 2),
+                new MyTask((startPoint + finishPoint) / 2, finishPoint)
+        );
     }
 }
