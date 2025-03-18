bc086c9159f589c6f5a524496b2bc985cffec3cb
Kate892F <tophours13@gmail.com>
2025-03-19 00:11:12 +0200

    task done

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -5,6 +5,7 @@ import java.util.concurrent.RecursiveTask;
 public class MyTask extends RecursiveTask<Long> {
     private int startPoint;
     private int finishPoint;
+    private int threshold = 2;
 
     public MyTask(int startPoint, int finishPoint) {
         this.startPoint = startPoint;
@@ -13,7 +14,34 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+
+        if (finishPoint - startPoint <= threshold) {
+            System.out.printf("Calculating sum from %s to %s. Thread: %s%n",
+                    startPoint, finishPoint, Thread.currentThread().getName());
+
+            return (long) calculateSum(startPoint, finishPoint);
+        } else {
+            System.out.printf("Splitting the task with start: %s, finish: %s. Thread: %s%n",
+                    startPoint, finishPoint, Thread.currentThread().getName());
+
+            int middle = startPoint + (finishPoint - startPoint) / 2;
+            MyTask leftTask = new MyTask(startPoint, middle);
+            MyTask rightTask = new MyTask(middle, finishPoint);
+
+            leftTask.fork();
+            rightTask.fork();
+
+            Long rightResult = rightTask.join();
+            Long leftResult = leftTask.join();
+            return rightResult + leftResult;
+        }
+    }
+
+    private int calculateSum(int start, int finish) {
+        int sum = 0;
+        for (int i = start; i < finish; i++) {
+            sum += i;
+        }
+        return sum;
     }
 }
