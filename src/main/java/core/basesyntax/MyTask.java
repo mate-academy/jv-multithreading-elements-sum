ade4074be6cd63358f627e0b6a7cc5ccdcb26a6a
Igor Neminuschii <igorgorslav@gmail.com>
2025-03-21 12:01:29 +0200

    implemented compute() method

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,8 +1,11 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
+    private int difference;
     private int startPoint;
     private int finishPoint;
 
@@ -13,7 +16,32 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        difference = finishPoint - startPoint;
+        long sum = 0L;
+
+        if (difference > 10) {
+            List<RecursiveTask<Long>> subTasks = new ArrayList<>(createSubTasks());
+            for (RecursiveTask<Long> subTask : subTasks) {
+                subTask.fork();
+            }
+            for (RecursiveTask<Long> subTask : subTasks) {
+                sum += subTask.join();
+            }
+        } else {
+            for (int i = startPoint; i < finishPoint; i++) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    private List<RecursiveTask<Long>> createSubTasks() {
+        List<RecursiveTask<Long>> subTasks = new ArrayList<>();
+        int middle = startPoint + difference / 2;
+        RecursiveTask<Long> subTask1 = new MyTask(startPoint, middle);
+        RecursiveTask<Long> subTask2 = new MyTask(middle, finishPoint);
+        subTasks.add(subTask1);
+        subTasks.add(subTask2);
+        return subTasks;
     }
 }
