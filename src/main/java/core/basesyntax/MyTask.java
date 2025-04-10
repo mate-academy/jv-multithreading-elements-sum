ec03a3207370b301cccbc8b509b29caf230a6cd5
JaverJaver <mopasaj205@arpizol.com>
2025-04-10 23:43:35 +0300

    fork join pool

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,8 +1,10 @@
 package core.basesyntax;
 
 import java.util.concurrent.RecursiveTask;
+import java.util.stream.LongStream;
 
 public class MyTask extends RecursiveTask<Long> {
+    private final int THRESHOLD = 10;
     private int startPoint;
     private int finishPoint;
 
@@ -13,7 +15,24 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        if (finishPoint - startPoint < THRESHOLD) {
+            return findSum(startPoint, finishPoint);
+        } else {
+            int middle = (startPoint + finishPoint) / 2;
+            MyTask leftTask = new MyTask(startPoint, middle);
+            MyTask rightTask = new MyTask(middle, finishPoint);
+
+            leftTask.fork();
+
+            Long right = rightTask.compute();
+            Long left = leftTask.join();
+
+            return left + right;
+        }
+    }
+
+    private long findSum(int startPoint, int finishPoint) {
+        return LongStream.range(startPoint, finishPoint)
+                .sum();
     }
 }
