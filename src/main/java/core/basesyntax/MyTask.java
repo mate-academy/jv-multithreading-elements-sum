9d2fdc42d413b19f6ed4aa8d84e9568a02a2d53a
Mateusz <mateusz.kiryk@o2.pl>
2025-05-14 21:12:38 +0200

    First solution

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
-    private int startPoint;
-    private int finishPoint;
+    private static final int THRESHOLD = 10;
+    private final int startPoint;
+    private final int finishPoint;
 
     public MyTask(int startPoint, int finishPoint) {
         this.startPoint = startPoint;
@@ -13,7 +14,20 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        if (finishPoint - startPoint <= THRESHOLD) {
+            long sum = 0;
+            for (int i = startPoint; i < finishPoint; i++) {
+                sum += i;
+            }
+            return sum;
+        } else {
+            int mid = startPoint + (finishPoint - startPoint) / 2;
+            MyTask leftTask = new MyTask(startPoint, mid);
+            MyTask rightTask = new MyTask(mid, finishPoint);
+            leftTask.fork();
+            Long rightResult = rightTask.compute();
+            Long leftResult = leftTask.join();
+            return leftResult + rightResult;
+        }
     }
 }
