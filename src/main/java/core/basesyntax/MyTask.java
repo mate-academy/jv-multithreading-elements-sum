f4c7955d90614c12a78ab38cf5aa3f78c63ecb97
Denys Mazurenko <0gur4ik3992@gmail.com>
2025-05-22 23:29:22 +0300

    Solve task with RecursiveTask

--- src/main/java/core/basesyntax/MyTask.java
+++ src/main/java/core/basesyntax/MyTask.java
@@ -1,8 +1,12 @@
 package core.basesyntax;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.RecursiveTask;
 
 public class MyTask extends RecursiveTask<Long> {
+    private static final int THRESHOLD = 10;
     private int startPoint;
     private int finishPoint;
 
@@ -13,7 +17,33 @@ public class MyTask extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        // write your code here
-        return null;
+        var distance = finishPoint - startPoint;
+        if (distance <= THRESHOLD) {
+            return calculateSum(startPoint, finishPoint);
+        } else {
+            List<RecursiveTask<Long>> tasks = initSubTasks();
+            tasks.forEach(ForkJoinTask::fork);
+            return tasks.stream()
+                    .mapToLong(ForkJoinTask::join)
+                    .sum();
+        }
+    }
+
+    private List<RecursiveTask<Long>> initSubTasks() {
+        var middle = (startPoint + finishPoint) / 2;
+        List<RecursiveTask<Long>> tasks = new LinkedList<>();
+        var firstTask = new MyTask(startPoint, middle);
+        var secondTask = new MyTask(middle, finishPoint);
+        tasks.add(firstTask);
+        tasks.add(secondTask);
+        return tasks;
+    }
+
+    private long calculateSum(int start, int finish) {
+        var sum = 0;
+        for (int i = start; i < finish; i++) {
+            sum += i;
+        }
+        return sum;
     }
 }
